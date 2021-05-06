e35b9a89d3bfec50b642276a5d585795e3729c10
Victor Hugo <victorh5900@gmail.com>
2021-05-06 17:07:04 -0400

    a

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -4,8 +4,8 @@ const NavBar = () => {
     return (
         <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadow-sm">
             <div className="container">
-                <nav className="my-2 my-md-0 mr-md-3">
-                    <img src={ImgDsDark} alt="DevSuperior" width="120" />
+                <nav className="my-2 my-md-0 mr-md-3" >
+                    <img src={ImgDsDark} alt="DevSuperior" width="150" />
                 </nav>
             </div>
         </div>
